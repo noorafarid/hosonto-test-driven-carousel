346810ce6dbed22d114b40d18d60c0d6b0a1c095
Noor Farid <nf@sadaqawf.onmicrosoft.com>
2021-05-12 14:50:04 +1000

    :construction: Starting work on CarouselButton

---
+++ wallaby.js
@@ -0,0 +1,14 @@
+module.exports = function(wallaby) {
+  return {
+  testFramework: 'jest',
+  env: {
+    type:'node',
+    tests: ['src/tests/**/*.test.js'],
+    files: ['src/**/*.js', '!**/*.test.js', '!**/.*'],
+    compilers: {
+      '**/*.js': wallaby.compilers.babel();
+    },
+  },
+};
+    
+  
